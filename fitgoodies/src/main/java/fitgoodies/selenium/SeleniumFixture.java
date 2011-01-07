bc8257ff2816e2d51dde2366338e2fcd737627b7
kia69 <kia69@37077cf2-5fae-4b09-84ff-c883f77c77cf>
2011-01-07 15:14:39 +0000

    documentation fixes. path to mvn.bat in build.xml fixed.
    git-svn-id: https://fitgoodies.svn.sourceforge.net/svnroot/fitgoodies/trunk@41 37077cf2-5fae-4b09-84ff-c883f77c77cf

--- fitgoodies/src/main/java/fitgoodies/selenium/SeleniumFixture.java
+++ fitgoodies/src/main/java/fitgoodies/selenium/SeleniumFixture.java
@@ -10,11 +10,11 @@ import fitgoodies.ActionFixture;
  * Run the selenium-IDE and record your test-case. 
  * Save the result as html and copy the table part into a new Html-File.
  * Adjust the first row and add the reference to this class. 
- * <table>
+ * <table border="1">
  * 		<tr><td>fitgoodies.selenium.SeleniumFixture</td><td></td><td></td></tr>
- * 		<tr><td>open</td><td>/application/mainMenu.html</td><td></td></tr>
- * 		<tr><td>type</td><td>j_username</td><td>username</td></tr>
- * 		<tr><td>type</td><td>j_password</td><td>secret</td></tr>
+ * 		<tr><td>open</td><td>/application/login.html</td><td></td></tr>
+ * 		<tr><td>type</td><td>id_username</td><td>username</td></tr>
+ * 		<tr><td>type</td><td>id_password</td><td>secret</td></tr>
  * 		<tr><td>clickAndWait</td><td>//input[@name='login']</td><td></td></tr>
  * 		<tr><td>clickAndWait</td><td>link=Products</td><td></td></tr>
  * </table>
